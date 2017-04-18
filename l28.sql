67f6fa7ff71f3c0986d3f0d4136d5fc86a301845
Justin Mulvaney <justin@crystalknows.com>
2017-04-18 18:19:23 -0400

    Update l28.sql

--- l28.sql
+++ l28.sql
@@ -1,1 +1,13 @@
+/* used to create a view for measuring customer engagement-- 
+assigns each customer a score for # of days product was used in the last 28 */
 
+CREATE VIEW customer_l28 AS (
+select
+  u.id,
+  count(distinct date_trunc('day', e.collector_tstamp) AS l28
+from
+  atomic.users u join atomic.events e ON u.id=e.user_id
+where
+  date_trunc('day', e.collector_tstamp) > current_date - Interval '28 days' 
+  AND u.plan_id!=1        
+)        
